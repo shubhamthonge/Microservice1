36c80ef0285ad63c2d1b8ce44daade704c2a2a9f
shubhamthonge <shubhamthonge131@gmail.com>
2024-04-11 11:46:58 +0530

    update

--- src/main/java/com/tech/Microservice1Application.java
+++ src/main/java/com/tech/Microservice1Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Microservice1Application {
+public class Microservice1Application {// push the code
 
 	public static void main(String[] args) {
 		SpringApplication.run(Microservice1Application.class, args);
